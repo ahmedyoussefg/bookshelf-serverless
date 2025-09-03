401a5ea5bbae261d0b4c66412d809dc039d8092b
ahmedyoussefg <108885178+ahmedyoussefg@users.noreply.github.com>
2025-09-03 13:00:44 +0300

    Use starred GSI in getUserBooks

--- backend/lambdas/books/get-user-books.ts
+++ backend/lambdas/books/get-user-books.ts
@@ -5,16 +5,32 @@ export const handler = async (event: APIGatewayEvent) => {
     // Get user ID from authorizer
     const userId = event.requestContext.authorizer?.principalId;
     const PK = `USER#${userId}`;
+
+    const hasStarredFilter = event.queryStringParameters?.starred === 'true';
     try {
-        const results = await dynamo.query({
-            TableName: process.env.DB_TABLE_NAME,
-            KeyConditionExpression: "PK=:PK and begins_with(SK, :skPrefix)",
-            ExpressionAttributeValues: {
-                ":PK": PK,
-                ":skPrefix": "BOOK#",
-            },
-            ScanIndexForward: false,
-        });
+        let results;
+        if (hasStarredFilter){
+            // Use GSI to get only starred books
+            results = await dynamo.query({
+                TableName: process.env.DB_TABLE_NAME,
+                IndexName: process.env.STARRED_BOOKS_INDEX_NAME,
+                KeyConditionExpression: "PK = :PK",
+                ExpressionAttributeValues: {
+                    ":PK": PK,
+                },
+                ScanIndexForward: false,
+            });
+        } else {
+            results = await dynamo.query({
+                TableName: process.env.DB_TABLE_NAME,
+                KeyConditionExpression: "PK=:PK and begins_with(SK, :skPrefix)",
+                ExpressionAttributeValues: {
+                    ":PK": PK,
+                    ":skPrefix": "BOOK#",
+                },
+                ScanIndexForward: false,
+            });
+        }
 
         const books = results.Items?.map(item => ({
             id: item.bookId,                  // rename bookId to id
